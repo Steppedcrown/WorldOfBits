001c8bcb149c8fa7be48e59cf24a048aec5bf3ef
Steppedcrown <92701553+Steppedcrown@users.noreply.github.com>
2025-11-10 21:04:52 +0000

    Cleaned up cell class

--- src/main.ts
+++ src/main.ts
@@ -46,48 +46,48 @@ class Cell {
     ]);
 
     this.rectangle = leaflet.rectangle(this.bounds).addTo(map);
+
+    // Handle token collection, placement, and merging
     this.rectangle.on("click", () => {
       if (player.heldToken) {
-        if (!this.token) {
-          this.setToken(player.heldToken);
-          player.heldToken = undefined;
-          updateStatus();
-        } else if (this.token === player.heldToken) {
-          this.setToken(this.token * 2);
-          player.heldToken = undefined;
-          updateStatus();
-        }
+        if (!this.token) this.setToken(player.heldToken);
+        else if (this.token === player.heldToken) this.setToken(this.token * 2);
+        player.heldToken = undefined;
+        updateStatus();
       } else if (this.token) {
         player.heldToken = this.token;
         this.setToken(undefined);
         updateStatus();
       }
     });
+
+    // Create random token assignment and value
     if (luck([i, j, "token-exists"].toString()) < 0.5) {
       this.token = luck([i, j, "token-value"].toString()) < 0.5 ? 1 : 2;
-      this.text = leaflet.marker(this.bounds.getCenter(), {
-        icon: leaflet.divIcon({
-          className: "token-label",
-          html: `<div>${this.token}</div>`,
-        }),
-      }).addTo(map);
+      this.setText();
     }
   }
 
   setToken(value: number | undefined) {
     this.token = value;
+
     if (this.text) {
       this.text.remove();
       this.text = undefined;
     }
+
     if (this.token) {
-      this.text = leaflet.marker(this.bounds.getCenter(), {
-        icon: leaflet.divIcon({
-          className: "token-label",
-          html: `<div>${this.token}</div>`,
-        }),
-      }).addTo(map);
+      this.setText();
     }
+  }
+
+  setText() {
+    this.text = leaflet.marker(this.bounds.getCenter(), {
+      icon: leaflet.divIcon({
+        className: "token-label",
+        html: `<div>${this.token}</div>`,
+      }),
+    }).addTo(map);
   }
 }
 // #endregion
